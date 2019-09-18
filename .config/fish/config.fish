e650076714e2fb636ef73689c77c3d5d576f5d90
James Walmsley <james@fullfat-fs.co.uk>
2019-09-18 12:49:14 +0100

    [FISH] Added initial fish config.

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+set fish_function_path $fish_function_path  "~/.local/lib/python3.6/site-packages/powerline/bindings/fish"
+source ~/.local/lib/python3.6/site-packages/powerline/bindings/fish/powerline-setup.fish
+powerline-setup
+
+alias code="code-insiders --force-device-scale-factor=1.25"
+
+alias cf="vim ~/.config/fish/config.fish"
+alias swc="vim ~/.config/sway/config"
+
+alias h="env GIT_WORK_TREE=$HOME GIT_DIR=$HOME/develop/.dot-files.git"
+
