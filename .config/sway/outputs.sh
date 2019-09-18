c28fd35ed66f0008f9e698b169d4deb60eb6e845
James Walmsley <james@fullfat-fs.co.uk>
2019-09-18 15:37:19 +0100

    [SWAY] Updated monitor outputs.

--- .config/sway/outputs.sh
+++ .config/sway/outputs.sh
@@ -2,8 +2,14 @@
 
 HOME_OFFICE=$(swaymsg -t get_outputs -p | grep Goldstar)
 
+echo $HOME_OFFICE
 
 if [ -n "$HOME_OFFICE" ]; then
+echo Configuring home office monitors.
+swaymsg output eDP-1 resolution 2560x1440 position \'0,0\' enable
+swaymsg output DP-4 resolution 3840x2160 position \'2560,0\' enable
+
+else
 
 echo Configuring GC Office Monitors.
 
@@ -13,11 +19,6 @@ swaymsg output DP-4 disable
 
 swaymsg output HDMI-A-1 resolution 3840x2160 position \'0,0\' enable
 
-else
-
-echo Configuring home office monitors.
-
-swaymsg output eDP-1 resolution 3840x2160 position \'0,0\' enable
 
 
 fi
