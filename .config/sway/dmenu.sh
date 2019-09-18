6b37fa287df5b2e698f1f597ced77835dcf32e18
James Walmsley <james@fullfat-fs.co.uk>
2019-09-18 17:41:14 +0100

    [SWAY] Auto switch outputs on startup - based on context.

--- .config/sway/dmenu.sh
+++ .config/sway/dmenu.sh
@@ -1,5 +1,19 @@
+#!/bin/bash
+
 export PATH="/snap/bin/:/var/lib/snapd/desktop:$PATH"
 
-rofi -combi-modi window,drun,run -show combi -modi combi
+#rofi -combi-modi window,drun,run -show combi -modi combi
 
+SCREEN_POS=$(swaymsg -t get_outputs \
+	| jq -r \
+	'.[] | select(.focused).rect | "\(.width)x\(.height)\\+\(.x)\\+\(.y)"')
+
+# GNU grep
+XWAYLAND_OUTPUT=$(xrandr | grep -oP "[A-Z]+[0-9]+(?= connected $SCREEN_POS)")
+
+MONITOR_ID=$(rofi --help | grep $XWAYLAND_OUTPUT -B1 \
+	| sed -sr '/ID/!d;s/[^:]*:\s([0-9])/\1/')
+
+# stdin for dmenu mode
+rofi -m $MONITOR_ID -combi-modi window,drun,run -show combi -modi combi </dev/stdin
 
--- .config/sway/outputs.sh
+++ .config/sway/outputs.sh
@@ -5,9 +5,20 @@ HOME_OFFICE=$(swaymsg -t get_outputs -p | grep Goldstar)
 echo $HOME_OFFICE
 
 if [ -n "$HOME_OFFICE" ]; then
-echo Configuring home office monitors.
-swaymsg output eDP-1 resolution 2560x1440 position \'0,0\' enable
-swaymsg output DP-4 resolution 3840x2160 position \'2560,0\' enable
+
+	if [ -n "$(swaymsg -t get_outputs -p | grep 'Unknown 0x0792')" ]; then
+		echo P1-Laptop - Configuring home office monitors
+		swaymsg output \"Goldstar Company Ltd LG Ultra HD 0x0000661B\" resolution 3840x2160 position \'0,0\' enable
+		swaymsg output eDP-1 resolution 1920x1080 position \'3840,0\' enable
+	else
+
+	
+		echo Carbon-Laptop - Configuring home office monitors
+		swaymsg output eDP-1 resolution 2560x1440 position \'0,0\' enable
+		swaymsg output DP-4 resolution 3840x2160 position \'2560,0\' enable
+
+	fi
+
 
 else
 
