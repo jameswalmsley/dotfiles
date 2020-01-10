5d45a193d8900c4a8379a6426a3621d9cad13cf2
James Walmsley <james@fullfat-fs.co.uk>
2020-01-10 23:20:11 +0000

    [SWAY] Ensure dmenu can select correct XWAYLAND display when scaling.

--- .config/sway/dmenu.sh
+++ .config/sway/dmenu.sh
@@ -8,10 +8,19 @@ SCREEN_POS=$(swaymsg -t get_outputs \
 	| jq -r \
 	'.[] | select(.focused).rect | "\(.width)x\(.height)\\+\(.x)\\+\(.y)"')
 
-# GNU grep
-XWAYLAND_OUTPUT=$(xrandr | grep -oP "[A-Z]+[0-9]+(?= connected $SCREEN_POS)")
+echo $SCREEN_POS
+POSX=$(echo $SCREEN_POS | cut -d + -f2 | cut -d \\ -f1)
+echo $POSX
+POSY=$(echo $SCREEN_POS | cut -d + -f3)
+echo $POSY
 
-MONITOR_ID=$(rofi --help | grep $XWAYLAND_OUTPUT -B1 \
+# GNU grep
+XWAYLAND_OUTPUT=$(xrandr | grep -oP "[A-Z]+[0-9]+(?= connected $POS)")
+
+OUTPUT=$(xrandr | grep -oP "[A-Z]+[0-9]+(?= connected [0-9]+x[0-9]+\+$POSX\+$POSY)")
+echo $OUTPUT
+
+MONITOR_ID=$(rofi --help | grep $OUTPUT -B1 \
 	| sed -sr '/ID/!d;s/[^:]*:\s([0-9])/\1/')
 
 # stdin for dmenu mode
