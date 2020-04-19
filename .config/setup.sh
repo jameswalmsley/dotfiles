9942f132e8e67800c0d900eff12f9881a1316b3d
James Walmsley <james@fullfat-fs.co.uk>
2020-04-19 11:09:35 +0100

    [SETUP] Updated setup to install yarn for coc.

--- .config/setup.sh
+++ .config/setup.sh
@@ -4,18 +4,18 @@ set -x
 
 # Clone tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
+deb_packages="node.js tmux npm python3-pip yarn"
 
-deb_packages="node.js tmux npm python3-pip"
-
-
+sudo apt update
 sudo apt -y install ${deb_packages}
-
-
 
 pip3 install powerline-status
 
 # Python support for Coc (CocInstall coc-python)
 pip3 install jedi
+
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
