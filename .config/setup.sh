e83abb4eb6f68186a5223c397e746cd5d5c5dc4f
James Walmsley <james@fullfat-fs.co.uk>
2020-06-18 18:59:59 +0100

    [SETUP] Install OMF.

--- .config/setup.sh
+++ .config/setup.sh
@@ -23,3 +23,6 @@ pip3 install jedi
 npm install -g npx
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+curl -L https://get.oh-my.fish | fish
+
