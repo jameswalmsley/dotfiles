84c0698416a0040d82b9b17b71812e8851184d6b
James Walmsley <james@fullfat-fs.co.uk>
2020-12-14 00:12:26 +0000

    [TMUX] Fixing copy/paste with TMUX.

--- .tmux/yank.sh
+++ .tmux/yank.sh
@@ -14,17 +14,17 @@ copy_use_osc52_fallback=$(tmux show-option -gvq "@copy_use_osc52_fallback")
 
 # Resolve copy backend: pbcopy (OSX), reattach-to-user-namespace (OSX), xclip/xsel (Linux)
 copy_backend=""
-if is_app_installed pbcopy; then
-  copy_backend="pbcopy"
-elif is_app_installed reattach-to-user-namespace; then
-  copy_backend="reattach-to-user-namespace pbcopy"
-elif [ -n "${DISPLAY-}" ] && is_app_installed xsel; then
-  copy_backend="xsel -i --clipboard"
-elif [ -n "${DISPLAY-}" ] && is_app_installed xclip; then
-  copy_backend="xclip -i -f -selection primary | xclip -i -selection clipboard"
-elif [ -n "${copy_backend_remote_tunnel_port-}" ] && [ "$(ss -n -4 state listening "( sport = $copy_backend_remote_tunnel_port )" | tail -n +2 | wc -l)" -eq 1 ]; then
-  copy_backend="nc localhost $copy_backend_remote_tunnel_port"
-fi
+#if is_app_installed pbcopy; then
+  #copy_backend="pbcopy"
+#elif is_app_installed reattach-to-user-namespace; then
+  #copy_backend="reattach-to-user-namespace pbcopy"
+#elif [ -n "${DISPLAY-}" ] && is_app_installed xsel; then
+  #copy_backend="xsel -i --clipboard"
+#elif [ -n "${DISPLAY-}" ] && is_app_installed xclip; then
+  #copy_backend="xclip -i -f -selection primary | xclip -i -selection clipboard"
+#elif [ -n "${copy_backend_remote_tunnel_port-}" ] && [ "$(ss -n -4 state listening "( sport = $copy_backend_remote_tunnel_port )" | tail -n +2 | wc -l)" -eq 1 ]; then
+  #copy_backend="nc localhost $copy_backend_remote_tunnel_port"
+#fi
 
 # if copy backend is resolved, copy and exit
 if [ -n "$copy_backend" ]; then
