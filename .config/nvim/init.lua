060b01b4f3f7dbe833dc5ff25657e0c84f59c1f5
James Walmsley <james@fullfat-fs.co.uk>
2021-07-05 10:19:13 +0100

    [NVIM] Updated Lunarvim from upstream

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,14 +1,16 @@
-require('default-config')
-require('keymappings')
+require "default-config"
+require "keymappings"
 if io.open(CONFIG_PATH .. '/lv-config.lua', 'r') then
-	vim.cmd('luafile ' .. CONFIG_PATH .. '/lv-config.lua')
+  vim.cmd("luafile " .. CONFIG_PATH .. "/lv-config.lua")
 end
 require('environment')
-require('settings')
-require('plugins')
-require('lv-utils')
-require('lv-galaxyline')
-require('lv-treesitter')
-require('lv-which-key')
-require('lsp')
-if O.lang.emmet.active then require('lsp.emmet-ls') end
+require "settings"
+require "plugins"
+require "lv-utils"
+require "lv-galaxyline"
+require "lv-treesitter"
+require "lv-which-key"
+require "lsp"
+if O.lang.emmet.active then
+  require "lsp.emmet-ls"
+end
