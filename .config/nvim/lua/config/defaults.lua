cb42c916864ebeb987eab25cba89010c95c0f66e
James Walmsley <james@fullfat-fs.co.uk>
2021-09-27 18:28:44 +0100

    [NVIM] Updated Lunarvim from upstream

--- .config/nvim/lua/config/defaults.lua
+++ .config/nvim/lua/config/defaults.lua
@@ -645,12 +645,6 @@ lvim.lang = {
         },
         settings = {
           Lua = {
-            runtime = {
-              -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-              version = "LuaJIT",
-              -- Setup your lua path
-              path = vim.split(package.path, ";"),
-            },
             diagnostics = {
               -- Get the language server to recognize the `vim` global
               globals = { "vim", "lvim" },
@@ -663,7 +657,7 @@ lvim.lang = {
                 [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
               },
               maxPreload = 100000,
-              preloadFileSize = 1000,
+              preloadFileSize = 10000,
             },
           },
         },
