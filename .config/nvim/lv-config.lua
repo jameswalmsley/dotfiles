5e37c6e914c54db69068cd287e2c763036ee9896
James Walmsley <james@fullfat-fs.co.uk>
2021-10-04 17:19:06 +0100

    [NVIM] Updated Lunarvim from upstream

--- .config/nvim/lv-config.lua
+++ .config/nvim/lv-config.lua
@@ -1,6 +1,13 @@
 lvim.builtin.dashboard.active = true
 lvim.builtin.nvimtree.side = "right"
 lvim.format_on_save = true
+lvim.lang.cpp.formatters = {
+  {
+    exe = 'clang_format',
+    args = {},
+    cwd = vim.fn.expand("%:p:h")
+  }
+}
 
 lvim.keys.normal_mode["<C-j>"] = ":cnext<CR>"
 lvim.keys.normal_mode["<C-k>"] = ":cprev<CR>"
