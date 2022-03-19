bfa939e101a5715e4a3badd15bc0b966b15a3b78
James Walmsley <james@fullfat-fs.co.uk>
2022-03-19 22:03:46 +0000

    [NVIM] Updated Lunarvim from upstream

--- .config/nvim/lua/distro/config.lua
+++ .config/nvim/lua/distro/config.lua
@@ -1,25 +1,41 @@
+lvim.colorscheme = "catppuccin"
 lvim.builtin.alpha.active = true
 lvim.builtin.lualine.options.globalstatus = true
 lvim.builtin.nvimtree.side = "right"
 lvim.format_on_save = true
 
-
 lvim.keys.normal_mode["<C-j>"] = ":cnext<CR>"
 lvim.keys.normal_mode["<C-k>"] = ":cprev<CR>"
 
-vim.opt.relativenumber=true
+vim.opt.relativenumber = true
 
 lvim.builtin.lualine.options = {
-    component_separators = { left = "", right = "" },
-    section_separators = { left = "", right = "" },
+  component_separators = { left = "", right = "" },
+  section_separators = { left = "", right = "" },
 }
 lvim.builtin.lualine.sections.lualine_z = {
-  "location"
+  "location",
 }
 
 lvim.builtin.which_key.mappings["o"] = {
   name = "Octo",
   i = {
     c = {},
-  }
+  },
+}
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  {
+    exe = "stylua",
+  },
+  {
+    exe = "black",
+  },
+}
+
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  exe = "shellcheck",
+  args = { "--severity", "warning" },
 }
