7338e1655afa6f232cb9aeb959c9569dfc38f982
James Walmsley <james@fullfat-fs.co.uk>
2022-04-03 12:11:39 +0100

    [NVIM] Updated Lunarvim from upstream

--- .config/nvim/lua/distro/config.lua
+++ .config/nvim/lua/distro/config.lua
@@ -1,7 +1,7 @@
 lvim.colorscheme = "catppuccin"
 lvim.builtin.alpha.active = true
 lvim.builtin.terminal.active = true
-lvim.builtin.terminal.direction = "horizontal"
+lvim.builtin.terminal.direction = "float"
 lvim.builtin.treesitter.ensure_installed = "maintained"
 lvim.builtin.lualine.options.globalstatus = true
 lvim.builtin.nvimtree.setup.quit_on_open = true
