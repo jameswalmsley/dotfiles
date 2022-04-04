4a1f357e42e64cecfde944dd0e1c6f6a97431c7b
James Walmsley <james@fullfat-fs.co.uk>
2022-04-04 12:55:37 +0100

    [NVIM] Updated Lunarvim from upstream

--- .config/nvim/lua/distro/config.lua
+++ .config/nvim/lua/distro/config.lua
@@ -18,47 +18,51 @@ vim.opt.relativenumber = true
 
 
 lvim.builtin.lualine.options = {
-  component_separators = { left = "", right = "" },
-  section_separators = { left = "", right = "" },
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
 }
 lvim.builtin.lualine.sections.lualine_z = {
-  "location",
+    "location",
 }
 
 lvim.builtin.which_key.mappings["o"] = {
-  name = "Octo",
-  i = {
-    c = {},
-  },
+    name = "Octo",
+    i = {
+        c = {},
+    },
 }
 
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  {
-    exe = "stylua",
-  },
-  {
-    exe = "black",
-  },
-}
+function init()
+    local formatters = require "lvim.lsp.null-ls.formatters"
+    formatters.setup {
+        {
+            exe = "stylua",
+        },
+        {
+            exe = "black",
+        },
+    }
 
-local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {
-  exe = "shellcheck",
-  args = { "--severity", "warning" },
-  filetypes = "sh",
-}
+    local linters = require "lvim.lsp.null-ls.linters"
+    linters.setup {
+        exe = "shellcheck",
+        args = { "--severity", "warning" },
+        filetypes = "sh",
+    }
 
-local clangd_flags = {
-  "--background-index",
-  "--cross-file-rename",
-  "--completion-style=bundled",
-  "--header-insertion=never",
-  "--suggest-missing-includes",
-  "-j=8",
-  "--pch-storage=memory",
-}
+    local clangd_flags = {
+        "--background-index",
+        "--cross-file-rename",
+        "--completion-style=bundled",
+        "--header-insertion=never",
+        "--suggest-missing-includes",
+        "-j=8",
+        "--pch-storage=memory",
+    }
 
-require("lvim.lsp.manager").setup("clangd", {
-  cmd = { "clangd", unpack(clangd_flags)},
-})
+    require("lvim.lsp.manager").setup("clangd", {
+        cmd = { "clangd", unpack(clangd_flags)},
+    })
+end
+
+pcall(init)
