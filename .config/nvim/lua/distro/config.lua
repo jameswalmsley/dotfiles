01513ddd69c28f65fa9e1540368141a2c091570e
James Walmsley <jamesw@graphcore.ai>
2022-05-06 10:24:50 +0100

    [NVIM] Updated Lunarvim from upstream

--- .config/nvim/lua/distro/config.lua
+++ .config/nvim/lua/distro/config.lua
@@ -4,7 +4,6 @@ lvim.builtin.terminal.active = true
 lvim.builtin.terminal.direction = "float"
 lvim.builtin.treesitter.ensure_installed = "all"
 lvim.builtin.lualine.options.globalstatus = true
-lvim.builtin.nvimtree.setup.quit_on_open = true
 lvim.builtin.nvimtree.setup.view.side = "right"
 lvim.format_on_save = false
 lvim.builtin.project.manual_mode = true
@@ -65,4 +64,4 @@ function init()
     })
 end
 
--- pcall(init)
+pcall(init)
