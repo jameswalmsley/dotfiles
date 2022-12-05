52b727713f4f854e06c2e0189a77c3274045eb0d
James Walmsley <james@fullfat-fs.co.uk>
2022-12-05 11:01:00 +0000

    [NVIM] Updated Lunarvim from upstream

--- .config/nvim/lua/lvim/core/alpha.lua
+++ .config/nvim/lua/lvim/core/alpha.lua
@@ -4,17 +4,29 @@ function M.config()
   local lvim_dashboard = require "lvim.core.alpha.dashboard"
   local lvim_startify = require "lvim.core.alpha.startify"
   lvim.builtin.alpha = {
-    dashboard = { config = {}, section = lvim_dashboard.get_sections() },
-    startify = { config = {}, section = lvim_startify.get_sections() },
+    dashboard = {
+      config = {},
+      section = lvim_dashboard.get_sections(),
+      opts = { autostart = true },
+    },
+    startify = {
+      config = {},
+      section = lvim_startify.get_sections(),
+      opts = { autostart = true },
+    },
     active = true,
     mode = "dashboard",
   }
 end
 
-local function resolve_buttons(theme_name, entries)
+local function resolve_buttons(theme_name, button_section)
+  if button_section.val and #button_section.val > 0 then
+    return button_section.val
+  end
+
   local selected_theme = require("alpha.themes." .. theme_name)
   local val = {}
-  for _, entry in pairs(entries) do
+  for _, entry in pairs(button_section.entries) do
     local on_press = function()
       local sc_ = entry[1]:gsub("%s", ""):gsub("SPC", "<leader>")
       local key = vim.api.nvim_replace_termcodes(sc_, true, false, true)
@@ -23,6 +35,9 @@ local function resolve_buttons(theme_name, entries)
     local button_element = selected_theme.button(entry[1], entry[2], entry[3])
     -- this became necessary after recent changes in alpha.nvim (06ade3a20ca9e79a7038b98d05a23d7b6c016174)
     button_element.on_press = on_press
+
+    button_element.opts = vim.tbl_extend("force", button_element.opts, entry[4] or button_section.opts or {})
+
     table.insert(val, button_element)
   end
   return val
@@ -36,12 +51,17 @@ local function resolve_config(theme_name)
   for name, el in pairs(section) do
     for k, v in pairs(el) do
       if name:match "buttons" and k == "entries" then
-        resolved_section[name].val = resolve_buttons(theme_name, v)
+        resolved_section[name].val = resolve_buttons(theme_name, el)
       elseif v then
         resolved_section[name][k] = v
       end
     end
+
+    resolved_section[name].opts = el.opts or {}
   end
+
+  local opts = lvim.builtin.alpha[theme_name].opts or {}
+  selected_theme.config.opts = vim.tbl_extend("force", selected_theme.config.opts, opts)
 
   return selected_theme.config
 end
