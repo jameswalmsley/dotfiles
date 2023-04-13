fd91da7f84442fc86645127abfacdbd48858dad6
James Walmsley <james@fullfat-fs.co.uk>
2023-04-13 12:20:20 +0100

    nvim: Don't update nvimtree root.

--- .config/nvim/config.lua
+++ .config/nvim/config.lua
@@ -67,6 +67,7 @@ lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+lvim.builtin.nvimtree.setup.update_focused_file.update_root = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
@@ -183,3 +184,5 @@ lvim.builtin.treesitter.highlight.enable = true
 --     require("nvim-treesitter.highlight").attach(0, "bash")
 --   end,
 -- })
+--
+lvim.builtin.cmp.sources[2].entry_filter = nil
