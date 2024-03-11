206a2cb3797572538d3e814d6e75b75b6918a5f7
James Walmsley <james@fullfat-fs.co.uk>
2024-03-11 15:08:10 +0000

    nvim: latest lazyvim config

--- .config/nvim/lua/plugins/catpuccin.lua
+++ .config/nvim/lua/plugins/catpuccin.lua
@@ -3,12 +3,13 @@ return {
     "catppuccin/nvim",
     lazy = false,
     name = "catppuccin",
+    enabled = false,
   },
   -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "catppuccin",
+  --   },
+  -- },
 }
