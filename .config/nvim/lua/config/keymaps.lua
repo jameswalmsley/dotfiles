d54660ef2de04f234affc8ac291525e3d8f56afe
James Walmsley <james@fullfat-fs.co.uk>
2025-03-10 21:24:57 +0000

    update to latest lazyvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,11 +2,3 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set(
-  "n",
-  "<leader>sx",
-  require("telescope.builtin").resume,
-  { noremap = true, silent = true, desc = "Resume" }
-)
-
-vim.keymap.set("n", "<leader>e", "<cmd>Neotree reveal<CR>", { noremap = true, silent = true, desc = "Open Neotree" })
